fa717a7c89cd7721a91901037dfa52dad1572a2a
mfep <serfozo.lorinc@gmail.com>
2017-04-03 14:06:41 +0200

    remove possilble NullReferenceException

--- Source/Code/EditorPlugin/Modules/ButtonControl.cs
+++ Source/Code/EditorPlugin/Modules/ButtonControl.cs
@@ -94,7 +94,6 @@ namespace MFEP.Duality.Editor.Plugins.InputPlugin.Modules
 		private void removeButton_Click (object sender, EventArgs e)
 		{
 			InputManager.RemoveButton (btnName);
-			Parent.Focus ();
 		}
 
 		private void textBox1_KeyPress (object sender, KeyPressEventArgs e)
