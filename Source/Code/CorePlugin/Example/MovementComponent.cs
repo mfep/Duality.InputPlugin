c5b5f93ddc20c14cbc176e867644b0e2c52a4b88
mfep <serfozo.lorinc@gmail.com>
2017-04-07 22:54:20 +0200

    update keymapping

--- Source/Code/CorePlugin/Example/MovementComponent.cs
+++ Source/Code/CorePlugin/Example/MovementComponent.cs
@@ -11,10 +11,8 @@ namespace MFEP.Duality.Plugins.InputPlugin.Example
 		public void OnUpdate ()
 		{
 			var direction = Vector2.Zero;
-			if (InputManager.IsButtonPressed ("Right")) direction += Vector2.UnitX;
-			if (InputManager.IsButtonPressed ("Left")) direction -= Vector2.UnitX;
-			if (InputManager.IsButtonPressed ("Up")) direction -= Vector2.UnitY;
-			if (InputManager.IsButtonPressed ("Down")) direction += Vector2.UnitY;
+			direction += InputManager.GetAxis ("Horizontal") * Vector2.UnitX;
+			direction -= InputManager.GetAxis ("Vertical") * Vector2.UnitY;
 			GameObj.Transform.MoveByAbs (direction * MovementSpeed * Time.TimeMult);
 		}
 	}
