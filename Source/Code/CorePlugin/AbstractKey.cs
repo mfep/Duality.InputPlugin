fa89c754348642596bbb71020abe248c8866bb9a
Lőrinc Serfőző <serfozo.lorinc@gmail.com>
2018-08-27 19:01:13 +0200

    ToString () in AbstractKey subclasses

--- Source/Code/CorePlugin/AbstractKey.cs
+++ Source/Code/CorePlugin/AbstractKey.cs
@@ -21,6 +21,8 @@ namespace mfep.Duality.Plugins.InputPlugin
 		private Key key;
 		public Key Key { get => key; set => key = value; }
 
+		public override string ToString () => $"{typeof(KeyboardKey).Name}: {key}";
+
 		internal override bool IsHit => DualityApp.Keyboard.KeyHit (key);
 		internal override bool IsReleased => DualityApp.Keyboard.KeyReleased (key);
 		internal override bool IsPressed (float deadZone) => DualityApp.Keyboard.KeyPressed (key);
@@ -31,6 +33,8 @@ namespace mfep.Duality.Plugins.InputPlugin
 		private global::Duality.Input.MouseButton mouseButton;
 		public global::Duality.Input.MouseButton Button { get => mouseButton; set => mouseButton = value; }
 
+		public override string ToString () => $"{typeof(MouseButton).Name}: {mouseButton}";
+		
 		internal override bool IsHit => DualityApp.Mouse.ButtonHit (mouseButton);
 		internal override bool IsReleased => DualityApp.Mouse.ButtonReleased (mouseButton);
 		internal override bool IsPressed (float deadZone) => DualityApp.Mouse.ButtonPressed (mouseButton);
@@ -41,6 +45,8 @@ namespace mfep.Duality.Plugins.InputPlugin
 		private global::Duality.GamepadButton gamepadButton;
 		public global::Duality.GamepadButton Button { get => gamepadButton; set => gamepadButton = value; }
 
+		public override string ToString () => $"{typeof(GamepadButton).Name}: {gamepadButton}";
+		
 		internal override bool IsHit => DualityApp.Gamepads[0].ButtonHit (gamepadButton);
 		internal override bool IsReleased => DualityApp.Gamepads[0].ButtonReleased (gamepadButton);
 		internal override bool IsPressed (float deadZone) => DualityApp.Gamepads[0].ButtonPressed (gamepadButton);
@@ -51,6 +57,8 @@ namespace mfep.Duality.Plugins.InputPlugin
 		private global::Duality.Input.GamepadAxis gamepadAxis;
 		public global::Duality.Input.GamepadAxis Axis { get => gamepadAxis; set => gamepadAxis = value; }
 
+		public override string ToString () => $"{typeof(GamepadAxis).Name}: {gamepadAxis}";
+		
 		internal override bool IsHit => false;
 		internal override bool IsReleased => false;
 		internal override bool IsPressed (float deadZone) => MathF.Abs (DualityApp.Gamepads[0].AxisValue (gamepadAxis)) > deadZone;
